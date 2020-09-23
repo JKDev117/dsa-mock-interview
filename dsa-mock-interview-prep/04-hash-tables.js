26c4cee6487e421aa0a4d9b44e4ae3185ede06c5
Jae Kim <jckim81@yahoo.com>
2020-09-22 22:40:42 -0400

    modified 04-hash-tables.js

--- dsa-mock-interview-prep/04-hash-tables.js
+++ dsa-mock-interview-prep/04-hash-tables.js
@@ -2,3 +2,24 @@
 Explain how a hash table works.
 */
 
+/*
+
+Hash tables (a.k.a. hash maps) are used to store key-value pairs. They are a collection of key-value pairs.
+
+They are like arrays, but they are not ordered.
+
+An object in JavaScript is basically a hash table.
+
+To implement a hash table, you can use an array to store data. You then hash the keys.
+
+A hash function converts keys into valid array indices.
+
+Hash tables can find values quickly given a key.
+
+Hash tables can add new-key-values quickly.
+
+A good hash should be fast, distribute keys uniformly, and be deterministic.
+
+Separate chaining and linear probing are two strategies used to deal with 2 keys that hash to the same index.
+
+*/
